eb6aac62d27a6844ded9157350043687e14fa117
pavan1419 <ptechhat@gmail.com>
2024-05-05 08:39:50 -0400

    abc

--- arm.sh
+++ arm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function start_arm {
-		echo -e "\n$GREEN*$BLUE Starting Anonymizing Relay Monitor (arm)..."$RESETCOLOR
+		echo -e "\n$GREEN*$BLUE Starting Anonymizing Relay Monitor (arm)..." $RESETCOLOR
 		xhost +
 		sleep 1
 		DISPLAY=:0.0 gnome-terminal -e "sudo arm"
